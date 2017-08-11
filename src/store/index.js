7d72ba3057ffaec4d7493292246ca911a39b3f68
drank <chenfeng_lian@126.com>
2017-08-11 15:59:10 +0800

    test

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import actions from './action'
+import getters from './getters'
+
+Vue.use(Vuex)
+
+const state = {
+	userInfo: null,
+	cart: null,
+	products: null		
+}
+
+export default new Vuex.Store({
+	state,
+	getters,
+	mutations,
+	actions
+})
